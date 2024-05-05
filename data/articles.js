bd5e0d8a615c791c14260d7fd3e009c03397d466
Adwai-T <waitforadi@gmail.com>
2024-05-05 12:55:17 +0530

    Changed Color Scheme, removed articles page

--- data/articles.js
+++ data/articles.js
@@ -1,10 +1,10 @@
 export const ARTICLES = [
-  {
-    'title': 'Angular',
-    'desc': 'Angular is a popular framework developed by google, used to create single page applications',
-    'tags': 'Web-Dev, Framework',
-    'image': '/images/lang/angular.svg',
-    'link': '/notes/angular.html',
-    'date': ''
-  },
+  // {
+  //   'title': 'Angular',
+  //   'desc': 'Angular is a popular framework developed by google, used to create single page applications',
+  //   'tags': 'Web-Dev, Framework',
+  //   'image': '/images/lang/angular.svg',
+  //   'link': '/notes/angular.html',
+  //   'date': ''
+  // },
 ];
