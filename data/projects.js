54cde33555054ea1459ad0b581c5b972029bd114
Adwai-T <waitforadi@gmail.com>
2024-06-24 12:13:44 +0530

    Changed about page to fix summary, and removed unused project links

--- data/projects.js
+++ data/projects.js
@@ -7,17 +7,25 @@ export const PROJECTS = [
     'link': '/projects/md-to-html/convert.html',
   },
   {
-    'title': 'Computer Vision',
-    'desc': 'Computer Vision and OpenCV examples',
+    'title': 'Check Others On Github',
+    'desc': 'Check other project that I have worked on, on github',
     'tags': 'computer vision, opencv, image transformation',
-    'image': '/images/lang/opencv.png',
-    'link': '/projects/computer-vision/image-transform.html',
+    'image': '/images/utils/cog.svg',
+    'link': 'https://github.com/Adwai-T',
+    'target': '_blank'
   },
-  {
-    'title': '2D Lightning',
-    'desc': '2D Lightning Demo',
-    'tags': '2d games, 2d lighting',
-    'image': '/images/lang/opencv.png',
-    'link': '/projects/lighting-2d/lightning.html',
-  }
+  // {
+  //   'title': 'Computer Vision',
+  //   'desc': 'Computer Vision and OpenCV examples',
+  //   'tags': 'computer vision, opencv, image transformation',
+  //   'image': '/images/lang/opencv.png',
+  //   'link': '/projects/computer-vision/image-transform.html',
+  // },
+  // {
+  //   'title': '2D Lightning',
+  //   'desc': '2D Lightning Demo',
+  //   'tags': '2d games, 2d lighting',
+  //   'image': '/images/lang/opencv.png',
+  //   'link': '/projects/lighting-2d/lightning.html',
+  // }
 ];
